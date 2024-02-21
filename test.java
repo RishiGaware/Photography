b9620f3508282221201383a0bf4ce4f70a9c1eb6
Rushikesh Gaware <109101685+RishiGaware@users.noreply.github.com>
2024-02-21 14:31:17 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test{
+
+    public void show(){
+        System.out.println("Hello");
+    }
+    public void display(){
+        System.out.println("World");
+    }
+
+    public static void main( String [] args){
+        test t = new test();
+    }
+}
